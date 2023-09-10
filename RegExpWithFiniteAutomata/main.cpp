ad6b811f9af3da2401d8b5c687ab4df1b6e35e17
iran13165 <45335488+iran13165@users.noreply.github.com>
2023-09-11 01:51:33 +0530

    implementation of dfa table added

--- RegExpWithFiniteAutomata/main.cpp
+++ RegExpWithFiniteAutomata/main.cpp
@@ -16,20 +16,22 @@ int main(int argc, const char * argv[]) {
     Builders build;
     NFA* a = build._char("a");
     NFA* b = build._char("b");
-//    NFA* c = build._char("c");
+    NFA* c = build._char("c");
 //    NFA* d = build._char("d");
    // problem occurs when call build.alt(build.charc("a"),build.charc("a"),build.charc("a"));
-    build.altPair(a,b);
+     NFA* in = build._or(a,b,c);
 //    build.alt(a,b,c);
 //    build.alt(a,b,c,d);
    // build.alt(a,b);
 
-    a->setTransitionTable(a->in);
-    a->setEpsilonClosure();
-
-    cout<<a->matches("abc")<<endl;
-    cout<<a->matches("b")<<endl;
-    cout<<a->matches("c")<<endl;
-    cout<<a->matches("d")<<endl;
+    in->setTransitionTable(in->in);
+    in->setEpsilonClosure();
+    in->getAcceptingStateNumbers();
+    in->getAlphabet();
+    in->create_dfa_table();
+    cout<<in->matches("abc")<<endl;
+    cout<<in->matches("b")<<endl;
+    cout<<in->matches("c")<<endl;
+    cout<<in->matches("d")<<endl;
     return 0;
 }
--- RegExpWithFiniteAutomata/nfa.cpp
+++ RegExpWithFiniteAutomata/nfa.cpp
@@ -28,15 +28,7 @@ public:
     bool matches(string str) {
       return this->in->match(str,this->in);
     }
-//    map<int,map<string,set<string>>> getTransitionTable()
-//    {
-//        map<int,map<string,set<string>>>_transitionTable = {};
-//        set<State*> visitedSet = {};
-//       // setTransitionTable(&_transitionTable,&visitedSet);
-//
-//
-//        return _transitionTable;
-//    }
+    set<State*> acceptingStates = set<State*>();
     map<unsigned long int,map<string,set<unsigned long int>>> *transitionTable = new (map<unsigned long int,map<string,set<unsigned long int>>>);
     set<State*> visitedSet;
     set<State*> getClosure(State* v_state)
@@ -74,6 +66,30 @@ public:
                    transitionTable->find(v_state->num)->second.insert(make_pair(EPSILON_CLOSURE, c_set));
                }
     }
+    set<string> getAlphabet()
+    {
+        set<string> alphabets = set<string>();
+        for(auto transition : *transitionTable)
+        {
+            for(auto list_tran : transition.second)
+            {
+                if(list_tran.first != EPSILON_CLOSURE)
+                {
+                    alphabets.insert(list_tran.first);
+                }
+            }
+        }
+        return alphabets;
+    }
+    set<unsigned long int> getAcceptingStateNumbers()
+    {
+        set<unsigned long int> setWithAcceptingStaeNumbers = set<unsigned long int>();
+        for(auto s : acceptingStates)
+        {
+            setWithAcceptingStaeNumbers.insert(s->num);
+        }
+        return  setWithAcceptingStaeNumbers;
+    }
    void setTransitionTable(State* start)
     {
             if(visitedSet.contains(start))
@@ -81,6 +97,10 @@ public:
                 return;
             }
             visitedSet.insert(start);
+            if(start->accepting == true)
+            {
+                acceptingStates.insert(start);
+            }
             start->num = visitedSet.size();
             transitionTable->insert({start->num,{}});
             map<string,set<State*>> transitions = start->getTransitions();
@@ -115,6 +135,194 @@ public:
                 }
             }
 
+    }
+    void create_dfa_table()
+    {
+        set<unsigned long int> starting_state = set<unsigned long int>();
+        map<string,set<unsigned long int>> it = transitionTable->at(1);
+        starting_state = it.at(EPSILON_CLOSURE);
+        
+        set<string> alphabets = getAlphabet();
+        set<unsigned long int> alphabetsSet;
+        map<string,map<string,set<unsigned long int>>> dfaTable = map<string,map<string,set<unsigned long int>>>();
+        map<string,set<unsigned long int>> transitionset = map<string,set<unsigned long int>>();
+        for(string alpha : alphabets)
+        {
+           
+            
+          for(auto st:starting_state)
+          {
+              
+             auto it = transitionTable->at(st).find(alpha);
+            
+              set<unsigned long int> tset = set<unsigned long int>();
+              if(it != transitionTable->at(st).end())
+              {
+                  alphabetsSet  = transitionTable->at(st).at(alpha);
+                  for(unsigned long int l : alphabetsSet)
+                  {
+                    set<unsigned long int> closerSet =  transitionTable->at(l).at(EPSILON_CLOSURE);
+                      
+                      for(unsigned long int closure : closerSet)
+                      {
+                          tset.insert(closure);
+                      }
+                  }
+               }
+              if(!tset.empty())
+              {
+                  auto it = transitionset.find(alpha);
+                  if(it != transitionset.end())
+                  {
+                      for(auto item:it->second)
+                      {
+                          it->second.insert(item);
+                      }
+                  } else
+                  {
+                      transitionset.insert({alpha,tset});
+                  }
+                                  
+              }
+          }
+          
+        
+        }
+        string key = "";
+        for(auto s :starting_state)
+        {
+            key+="_"+to_string(s);
+        }
+          auto itdfa = dfaTable.find(key);
+          if(itdfa != dfaTable.end())
+          {
+              for(auto item : itdfa->second)
+              {
+                  itdfa->second.insert(make_pair(item.first, item.second));
+              }
+          } else
+          {
+              dfaTable.insert({key,transitionset});
+          }
+        loop_for_dfa_table(&dfaTable, key);
+        remap_dfa_table(&dfaTable);
+        
+    }
+    void loop_for_dfa_table(map<string,map<string,set<unsigned long int>>>* dfaTable, string last_key)
+    {
+        set<string> alphabets = getAlphabet();
+        for(string alph:alphabets)
+        {
+            set<unsigned long int> starting_state = set<unsigned long int>();
+            auto it = dfaTable->at(last_key).find(alph);
+            if(it !=  dfaTable->at(last_key).end())
+            {
+                 starting_state = it->second;
+//                map<string,set<unsigned long int>> it = transitionTable->at(1);
+//                starting_state = it.at(EPSILON_CLOSURE);
+                
+               
+                set<unsigned long int> alphabetsSet;
+                map<string,set<unsigned long int>> transitionset = map<string,set<unsigned long int>>();
+                for(string alpha : alphabets)
+                {
+                   
+                    
+                  for(auto st:starting_state)
+                  {
+                      
+                     auto it = transitionTable->at(st).find(alpha);
+                    
+                      set<unsigned long int> tset = set<unsigned long int>();
+                      if(it != transitionTable->at(st).end())
+                      {
+                          alphabetsSet  = transitionTable->at(st).at(alpha);
+                          for(unsigned long int l : alphabetsSet)
+                          {
+                            set<unsigned long int> closerSet =  transitionTable->at(l).at(EPSILON_CLOSURE);
+                              
+                              for(unsigned long int closure : closerSet)
+                              {
+                                  tset.insert(closure);
+                              }
+                          }
+                       }
+                      if(!tset.empty())
+                      {
+                          auto it = transitionset.find(alpha);
+                          if(it != transitionset.end())
+                          {
+                              for(auto item:it->second)
+                              {
+                                  it->second.insert(item);
+                              }
+                          } else
+                          {
+                              transitionset.insert({alpha,tset});
+                          }
+                                          
+                      }
+                  }
+                  
+                
+                }
+                string key = "";
+                for(auto s :starting_state)
+                {
+                    key+="_"+to_string(s);
+                }
+                  auto itdfa = dfaTable->find(key);
+                  if(itdfa != dfaTable->end())
+                  {
+                      for(auto item : itdfa->second)
+                      {
+                          itdfa->second.insert(make_pair(item.first, item.second));
+                      }
+                  } else
+                  {
+                      dfaTable->insert({key,transitionset});
+                  }
+                loop_for_dfa_table(dfaTable,key);
+            }
+            
+        }
+        
+        
+        
+       
+       // unsigned long size =  dfaTable.size();
+    }
+    void remap_dfa_table(map<string,map<string,set<unsigned long int>>>* dfaTable)
+    {
+        map<unsigned long int,map<string,unsigned long int>> final_dfa = map<unsigned long int,map<string,unsigned long int>>();
+        map<string,unsigned long int> mapping = map<string,unsigned long int>();
+        unsigned long int increment = 1;
+        set<string> alphabets = getAlphabet();
+        for(auto transition : *dfaTable)
+        {
+            mapping[transition.first] = increment;
+            increment++;
+        }
+        for(auto transition : *dfaTable)
+        {
+            for(auto alpha: alphabets){
+                 auto it = dfaTable->at(transition.first).find(alpha);
+                if(it != dfaTable->at(transition.first).end()){
+                    set<unsigned long int> sets = dfaTable->at(transition.first).at(alpha);
+                     string key = "";
+                     for(auto set : sets)
+                     {
+                         key+="_"+to_string(set);
+                     }
+                     final_dfa[mapping[transition.first]].insert({alpha,mapping[key]});
+
+                }
+                else{
+                    final_dfa[mapping[transition.first]].insert({});
+                }
+            }
+        }
+        
     }
 };
 #endif
